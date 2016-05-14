0b3e92be7a87c5837c379cb63f9f11aec717383a
d-fay <faydustin@gmail.com>
2016-05-14 15:29:02 -0600

    passing vars and props

--- public/app.jsx
+++ public/app.jsx
@@ -1,18 +1,33 @@
 // REACT COMPONENT (re-usable)
 var Greeter = React.createClass({
+  // provides default values if no properties are specifed
+  getDefaultProps: function () {
+    return {
+      name: 'React'
+    };
+  },
   render: function() {
+    // declare property var for use in JSX
+    var name = this.props.name;
     return (
       // can only return a single root HTML element/tag
+      // curly braces used for props (properties of component)
       <div>
-        <h1>Hello React!</h1>
+
+        <h1>Hello {name}!</h1>
         <p>This is rendered from a component</p>
       </div>
     );
   }
 });
 
+var nameTwo = 'dfay';
+
 // DOM (React component passed to DOM as first parameter)
 ReactDOM.render(
-  <Greeter/>,
+  // properties are passed into components this way (ie: name="d-fay")
+  // <Greeter name="d-fay"/>,
+  // or in a modular way by passing in vars like this!
+  <Greeter name={nameTwo}/>,
   document.getElementById('app')
 );
