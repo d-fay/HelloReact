6ffb8e5ae585eb10b20289c6cd54d05179925fde
d-fay <faydustin@gmail.com>
2016-05-14 14:43:43 -0600

    add modularity (implement component)

--- public/app.jsx
+++ public/app.jsx
@@ -1,4 +1,18 @@
+// REACT COMPONENT (re-usable)
+var Greeter = React.createClass({
+  render: function() {
+    return (
+      // can only return a single root HTML element/tag
+      <div>
+        <h1>Hello React!</h1>
+        <p>This is rendered from a component</p>
+      </div>
+    );
+  }
+});
+
+// DOM (React component passed to DOM as first parameter)
 ReactDOM.render(
-  <h1>Hello React!</h1>,
+  <Greeter/>,
   document.getElementById('app')
 );
