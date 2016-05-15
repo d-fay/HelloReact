816f19446f16d6191a67bea451ff479e0ef70d7c
d-fay <faydustin@gmail.com>
2016-05-14 19:33:10 -0600

    more playing with passing props and setting defaults

--- public/app.jsx
+++ public/app.jsx
@@ -3,31 +3,33 @@ var Greeter = React.createClass({
   // provides default values if no properties are specifed
   getDefaultProps: function () {
     return {
-      name: 'React'
+      name: 'React',
+      msg: 'This is a backup/default message prop'
     };
   },
   render: function() {
     // declare property var for use in JSX
     var name = this.props.name;
+    var message = this.props.msg;
     return (
       // can only return a single root HTML element/tag
       // curly braces used for props (properties of component)
       <div>
-
         <h1>Hello {name}!</h1>
-        <p>This is rendered from a component</p>
+        <p>{message + '!!'}</p>
       </div>
     );
   }
 });
-
-var nameTwo = 'dfay';
+// prop vars to pass to DOM
+var nameProp = 'dfay';
+var msgProp = 'This message param is passed as a prop'
 
 // DOM (React component passed to DOM as first parameter)
 ReactDOM.render(
   // properties are passed into components this way (ie: name="d-fay")
   // <Greeter name="d-fay"/>,
   // or in a modular way by passing in vars like this!
-  <Greeter name={nameTwo}/>,
+  <Greeter name={nameProp} msg={msgProp}/>,
   document.getElementById('app')
 );
