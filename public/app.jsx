c576d38bbafddc944f3467f5e87cc3511165dd86
d-fay <faydustin@gmail.com>
2016-05-17 21:02:29 -0600

    clearing text-field and adding conditional parameter updating

--- public/app.jsx
+++ public/app.jsx
@@ -7,9 +7,35 @@ var Greeter = React.createClass({
       msg: 'This is a backup/default message prop'
     };
   },
+  getInitialState: function () {
+    return {
+      // STATE IS INTERNALLY MAINTAINED AND UPDATED BY THE COMPONENT
+      // PROPS GET PASSED INTO A COMPONENT AS YOU INITIALIZE IT
+      name: this.props.name
+      // should be used to initialize, whereas
+      // name: this.state.name
+      // should be used to update
+      //
+      // NOTE: component cant update props, THEY CAN ONLY UPDATE STATE
+    };
+  },
+  onButtonClick: function (e) {
+    e.preventDefault();
+    // see refs in Greeter tag
+    var name = this.refs.name.value;
+    // clear the input text field
+    this.refs.name.value = '';
+
+    if (typeof name === 'string' && name.length > 0) {
+      // set the var name to name value
+      this.setState({
+        name: name
+      });
+    }
+  },
   render: function() {
     // declare property var for use in JSX
-    var name = this.props.name;
+    var name = this.state.name;
     var message = this.props.msg;
     return (
       // can only return a single root HTML element/tag
@@ -17,12 +43,18 @@ var Greeter = React.createClass({
       <div>
         <h1>Hello {name}!</h1>
         <p>{message + '!!'}</p>
+
+        <form onSubmit={this.onButtonClick}>
+          <input type="text" ref="name"/>
+          <button>Set Name</button>
+        </form>
+
       </div>
     );
   }
 });
 // prop vars to pass to DOM
-var nameProp = 'dfay';
+var nameProp = 'stranger';
 var msgProp = 'This message param is passed as a prop'
 
 // DOM (React component passed to DOM as first parameter)
